35d07e190f6f37620af75091879f6f7d727a3bd1
Brian Lu <brian@pololu.com>
2014-08-05 11:03:03 -0700

    more test

--- ruby/main.rb
+++ ruby/main.rb
@@ -7,19 +7,22 @@ astar = ArduinoFirmata.connect ARGV.shift
 puts "firmata version #{astar.version}"
 
 astar.pin_mode 13, ArduinoFirmata::OUTPUT
-astar.pin_mode 10, ArduinoFirmata::OUTPUT
-astar.pin_mode 8, ArduinoFirmata::OUTPUT
+astar.pin_mode 9, ArduinoFirmata::OUTPUT
+astar.pin_mode 7, ArduinoFirmata::OUTPUT
 
 stat = true
 loop do
   puts stat
   astar.digital_write 13, stat
   astar.digital_write 8, stat
-  0.upto(255) do |i|
+  0.upto(200) do |i|
+    astar.analog_write 10, i
+    sleep 0.1
+  end
+  200.upto(0) do |i|
     astar.analog_write 10, i
     sleep 0.1
   end
   stat = !stat
-  astar.analog_write 10, 0
   sleep 0.1
 end
