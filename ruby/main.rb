4e2a7b8d1deda6c987e1e60b98d5d661d39e2192
Brian Lu <brian@pololu.com>
2014-08-05 15:39:21 -0700

    fix.

--- ruby/main.rb
+++ ruby/main.rb
@@ -17,7 +17,11 @@ astar.pin_mode 9, ArduinoFirmata::OUTPUT #l_pwm
 astar.pin_mode 8, ArduinoFirmata::OUTPUT #r_dir
 astar.pin_mode 7, ArduinoFirmata::OUTPUT #l_dir
 
-m = Motors.new(astar, 9, 7, 10, 8)
+m = Motors.new(uc: astar,
+               left_motor_pwm_pin: 9,
+               left_motor_dir_pin: 7,
+               right_motor_pwm_pin: 10,
+               right_motor_dir_pin: 8)
 m.forward 100, 100
 sleep 1
 
