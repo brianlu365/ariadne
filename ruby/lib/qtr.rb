23953219b5f826c95e36ff319055dac7ed4006be
Brian Lu <brian@pololu.com>
2014-08-06 10:20:43 -0700

    work on qtr

---
+++ ruby/lib/qtr.rb
@@ -0,0 +1,35 @@
+class Qtr
+  attr_reader :l, :r, :f, :f_1, :f_2, :f_3
+  def initialize(args)
+    @uc = args[:uc]
+    @l_p = args[:left_pin]
+    @r_p = args[:right_pin]
+    @f_1_p = args[:front_1_pin]
+    @f_2_p = args[:front_2_pin]
+    @f_3_p = args[:front_3_pin]
+  end
+
+  def l
+    
+  end
+
+  def r
+
+  end
+
+  def f
+
+  end
+
+  def f_1
+
+  end
+
+  def f_2
+
+  end
+
+  def f_3
+
+  end
+end
--- ruby/motors_test.rb
+++ ruby/motors_test.rb
@@ -2,32 +2,23 @@
 $:.unshift File.expand_path './lib', File.dirname(__FILE__)
 require "rubygems"
 require "arduino_firmata"
-require "qtr"
+require "motors"
 
 astar = ArduinoFirmata.connect ARGV.shift
 puts "firmata version #{astar.version}"
 
 
-astar.pin_mode 13, ArduinoFirmata::OUTPUT #led
-# motors
+astar.pin_mode 13, ArduinoFirmata::OUTPUT
 astar.pin_mode 10, ArduinoFirmata::OUTPUT #r_pwm
 astar.pin_mode 9, ArduinoFirmata::OUTPUT #l_pwm
 astar.pin_mode 8, ArduinoFirmata::OUTPUT #r_dir
 astar.pin_mode 7, ArduinoFirmata::OUTPUT #l_dir
-# qtr
-astar.pin_mode 'A1', ArduinoFirmata::INPUT #r_qtr
-astar.pin_mode 'A2', ArduinoFirmata::INPUT #l_qtr
-astar.pin_mode 'A3', ArduinoFirmata::INPUT #f1_qtr
-astar.pin_mode 'A4', ArduinoFirmata::INPUT #f2_qtr
-astar.pin_mode 'A5', ArduinoFirmata::INPUT #f3_qtr
 
-
-q = Qtr.new(uc: astar,
-           left_pin: 12,
-           right_pin: 11,
-           front_1_pin: 6,
-           front_2_pin: 1,
-           front_3_pin: 10)
+m = Motors.new(uc: astar,
+               left_motor_pwm_pin: 9,
+               left_motor_dir_pin: 7,
+               right_motor_pwm_pin: 10,
+               right_motor_dir_pin: 8)
 0.upto 200 do |i|
   m.forward 100, 100
   sleep 0.1
@@ -52,4 +43,4 @@ sleep 0.2
 m.right 100
 sleep 1
 
-m.break
+m.close
