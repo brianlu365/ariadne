a2664e9861e3be76075f1fa872a7f9ce710206a6
Brian Lu <brianlu365@gmail.com>
2015-04-15 16:33:23 -0700

    adjustment

--- ruby/lib/robot.rb
+++ ruby/lib/robot.rb
@@ -4,7 +4,7 @@ class Robot
     @motors = args[:motors]
     @encoder = args[:encoder]
     @qtr = args[:qtr]
-    @speed = 40
+    @speed = 30
   end
 
   def go
@@ -21,19 +21,19 @@ class Robot
     until (@qtr.deadend?) do
       position = @qtr.position
       offset_from_center = position - 1000
-      power_difference = offset_from_center / 54
+      power_difference = offset_from_center / 70
 
       if power_difference > @speed || power_difference < -@speed
         power_difference = @speed
       end
 
-      puts "pos: #{position}, ofc: #{offset_from_center}, pd: #{power_difference}"
+      # puts "pos: #{position}, ofc: #{offset_from_center}, pd: #{power_difference}"
 
       if offset_from_center > 0
-        puts "adjust to right, #{@speed + power_difference}, #{@speed}"
+        # puts "adjust to right, #{@speed + power_difference}, #{@speed}"
         @motors.set_speed (@speed + power_difference), @speed
       else
-        puts "adjust to left, #{@speed}, #{@speed - power_difference}"
+        # puts "adjust to left, #{@speed}, #{@speed - power_difference}"
         @motors.set_speed @speed, (@speed - power_difference)
       end
       sleep 0.01
