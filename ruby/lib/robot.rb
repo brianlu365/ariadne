283ff38dd6cb1e2ccb0077d48258870e19f7e76f
Brian Lu <brianlu365@gmail.com>
2015-04-16 13:55:16 -0700

    m

--- ruby/lib/robot.rb
+++ ruby/lib/robot.rb
@@ -4,7 +4,7 @@ class Robot
     @motors = args[:motors]
     @encoder = args[:encoder]
     @qtr = args[:qtr]
-    @speed = 15
+    @speed = 30
   end
 
   def go
@@ -26,7 +26,7 @@ class Robot
       derivative = offset_from_center - last_offset_from_center
       last_offset_from_center = offset_from_center
 
-      power_difference = offset_from_center/4 + derivative*18
+      power_difference = offset_from_center/20 + derivative*10
 
       if power_difference > @speed || power_difference < -@speed
         power_difference = @speed
